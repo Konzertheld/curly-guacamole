3bc00f650572e27022138a6b967eb854c0034ba1
Konzertheld <Konzertheld@users.noreply.github.com>
2021-01-08 03:03:29 +0100

    initial database layout, resolves #4

---
+++ initdb.sql
@@ -0,0 +1,25 @@
+CREATE TABLE "tasks" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"description"	TEXT DEFAULT NULL COLLATE NOCASE,
+	"duration"	INTEGER NOT NULL,
+	"date"	INTEGER NOT NULL,
+	"advance_span"	INTEGER,
+	"deadline_day"	INTEGER DEFAULT NULL,
+	"deadline_time"	INTEGER DEFAULT NULL,
+	"recurrence_type"	INTEGER DEFAULT NULL,
+	"recurrence_days"	INTEGER DEFAULT NULL,
+	"done"	INTEGER DEFAULT 0,
+	"google_id"	TEXT DEFAULT NULL,
+	"rtm_id"	TEXT DEFAULT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE "tags" (
+	"name"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("name")
+);
+
+CREATE TABLE "tasks_tags" (
+	"task_id"	INTEGER NOT NULL,
+	"tag_name"	TEXT NOT NULL
+);
