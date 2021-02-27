14e6cab8fef31f30d9640525edf27efd95628e00
Konzertheld <Konzertheld@users.noreply.github.com>
2021-02-27 04:42:39 +0100

    Improve handling of shortcuts, reduce code. #1 Implement deleting and done+today (see also #9).

--- _api.php
+++ _api.php
@@ -6,16 +6,31 @@ require_once("_base.php");
 $conn = new PDO('sqlite:' . __DIR__ . '/data/data.db');
 $task_ids = explode(",", $_GET["task_ids"]);
 
-switch($_GET["command"]) {
-	case "done":
-		$done = $conn->prepare('UPDATE tasks SET done=((done | 1) - (done & 1)) WHERE id = :id');
-		$conn->beginTransaction();
-		foreach($task_ids as $task_id) {
-			$done->bindValue(':id', $task_id);
-			$done->execute();
-		}
-		echo $conn->commit();
-		break;
+foreach(explode(",", $_GET["commands"]) as $command) {
+	switch ($command) {
+		case "done":
+			$done = $conn->prepare('UPDATE tasks SET done=((done | 1) - (done & 1)) WHERE id = :id');
+			$conn->beginTransaction();
+			foreach ($task_ids as $task_id) {
+				$done->bindValue(':id', $task_id);
+				$done->execute();
+			}
+			echo $conn->commit();
+			break;
+		case "today":
+			foreach ($task_ids as $task_id) {
+				move_task($conn, $task_id, date("Y-m-d"));
+			}
+			break;
+		case "delete":
+			$done = $conn->prepare('DELETE FROM tasks WHERE id = :id');
+			$conn->beginTransaction();
+			foreach ($task_ids as $task_id) {
+				$done->bindValue(':id', $task_id);
+				$done->execute();
+			}
+			echo $conn->commit();
+			break;
+	}
 }
-
 echo "end";
--- ui.js
+++ ui.js
@@ -1,45 +1,56 @@
-window.onload = function() {
+window.onload = function () {
     // Select tasks
     $(".task h2").on("click", null, null, function (e) {
         $(this).parent().toggleClass("selected");
     });
 
     // Keyboard shortcuts
-    $("body").on("keypress", null, null, function(e) {
+    $("body").on("keypress", null, null, function (e) {
         // Which tasks operate on?
         task_ids = [];
-        $(".selected").each(function(){ task_ids.push(this.id.substring(5)); });
+        $(".selected").each(function () {
+            task_ids.push(this.id.substring(5));
+        });
 
         switch (e.which) {
             case 68:
                 // D for done and move to today
+                commands = ["done", "today"];
                 break;
             case 100:
                 // d for done
-                $.ajax({
-                        url: "/planer/_api.php",
-                        data: {
-                            task_ids: task_ids.join(","),
-                            command: "done"
-                        },
-                        success: function (result) {
-                            window.location.reload(true);
-                        }
-                    });
+                commands = ["done"];
+                break;
+            case 127:
+                // del
+                commands = ["delete"];
                 break;
             case 49:
-                // 1 for set duration to 1x 15min
+            // 1 for set duration to 1x 15min
             case 50:
-                // 2 for set duration to 2x 15min = 30min etc
+            // 2 for set duration to 2x 15min = 30min etc
             case 51:
             case 52:
             case 53:
             case 54:
+                commands = ["duration"];
                 break;
             case 32:
                 // space for abort all
                 $(".selected").removeClass("selected");
                 break;
+        }
+        if(typeof commands !== "undefined") {
+            $.ajax({
+                url: "/planer/_api.php",
+                data: {
+                    task_ids: task_ids.join(","),
+                    commands: commands.join(",")
+                },
+                success: function (result) {
+                    window.location.reload(true);
+                }
+            });
         }
     });
 };
