3f7ce16bbd9fbefd1a041107bc905b3fe0d09066
Konzertheld <Konzertheld@users.noreply.github.com>
2021-03-14 13:17:19 +0100

    remove useless comment

--- _google.php
+++ _google.php
@@ -1,7 +1,6 @@
 <?php
 function google_create_date($dateobj)
 {
-	// TODO: Use nullable operator
 	if (property_exists($dateobj, "dateTime")) {
 		return date_create($dateobj->dateTime);
 	} else return date_create($dateobj->date);
