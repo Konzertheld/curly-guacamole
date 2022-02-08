4f0ba1aa0f36baab53784269a10100b2c7996152
Konzertheld <Konzertheld@users.noreply.github.com>
2022-02-08 20:45:16 +0100

    remove autoincrement statement from create table. coding style. see https://www.sqlite.org/autoinc.html

--- setup/initdb.sql
+++ setup/initdb.sql
@@ -1,31 +1,38 @@
-CREATE TABLE "tasks" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"description"	TEXT DEFAULT NULL COLLATE NOCASE,
-	"duration"	INTEGER NOT NULL,
-	"date"	TEXT,
-	"advance_span"	INTEGER,
-	"deadline_day"	INTEGER DEFAULT NULL,
-	"deadline_time"	INTEGER DEFAULT NULL,
-	"recurrence_type"	INTEGER DEFAULT NULL,
-	"recurrence_days"	INTEGER DEFAULT NULL,
-	"done"	INTEGER DEFAULT 0,
-	"google_id"	TEXT DEFAULT NULL UNIQUE,
-	"rtm_id"	TEXT DEFAULT NULL UNIQUE,
-	PRIMARY KEY("id" AUTOINCREMENT)
+CREATE TABLE "tasks"
+(
+    "id"              INTEGER NOT NULL UNIQUE,
+    "description"     TEXT    DEFAULT NULL COLLATE NOCASE,
+    "duration"        INTEGER NOT NULL,
+    "date"            TEXT,
+    "advance_span"    INTEGER,
+    "deadline_day"    INTEGER DEFAULT NULL,
+    "deadline_time"   INTEGER DEFAULT NULL,
+    "recurrence_type" INTEGER DEFAULT NULL,
+    "recurrence_days" INTEGER DEFAULT NULL,
+    "done"            INTEGER DEFAULT 0,
+    "google_id"       TEXT    DEFAULT NULL UNIQUE,
+    "rtm_id"          TEXT    DEFAULT NULL UNIQUE,
+    PRIMARY KEY ("id")
 );
 
-CREATE TABLE "tags" (
-	"name"	TEXT NOT NULL UNIQUE,
-	PRIMARY KEY("name")
+CREATE TABLE "tags"
+(
+    "name" TEXT NOT NULL UNIQUE,
+    PRIMARY KEY ("name")
 );
 
-CREATE TABLE "tasks_tags" (
-	"task_id"	INTEGER NOT NULL,
-	"tag_name"	TEXT NOT NULL,
-	UNIQUE("task_id","tag_name")
+CREATE TABLE "tasks_tags"
+(
+    "task_id"  INTEGER NOT NULL,
+    "tag_name" TEXT    NOT NULL,
+    UNIQUE ("task_id", "tag_name")
 );
 
-INSERT INTO tags (name) VALUES ("overdue");
-INSERT INTO tags (name) VALUES ("additional");
-INSERT INTO tags (name) VALUES ("orphaned");
-INSERT INTO tags (name) VALUES ("expired");
+INSERT INTO tags (name)
+VALUES ("overdue");
+INSERT INTO tags (name)
+VALUES ("additional");
+INSERT INTO tags (name)
+VALUES ("orphaned");
+INSERT INTO tags (name)
+VALUES ("expired");
